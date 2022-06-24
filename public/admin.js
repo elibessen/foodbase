7e3434791ad9f2937a9352fd76851a74243ae429
Phoenexus22 <brwebber04@gmail.com>
2022-06-24 12:57:04 +0930

    buttons now load different content

--- public/admin.js
+++ public/admin.js
@@ -1,7 +1,23 @@
+var currentpage="shoppingListCont";
+var pagarray = [];
+
 function adminOnload()
 {
    var page = document.getElementById("page");
    console.log(page.style.display)
-   document.getElementById("admintest").innerHTML = "loaded!";
+   page.style.display = "flex";
+   pagarray["shoppingListCont"] = document.getElementById("shoppingListCont");
+   pagarray["studentOrdersCont"] = document.getElementById("studentOrdersCont");
+   pagarray["editOrdersCont"] = document.getElementById("editOrdersCont");
+   pagarray["addOrderCont"] = document.getElementById("addOrderCont");
+   pagarray["editIListsCont"] = document.getElementById("editIListsCont");
+   pagarray["editIngredientsCont"] = document.getElementById("editIngredientsCont");
+
 
 }
+function changepage(newpage)
+{
+   pagarray[currentpage].style.display = "none";
+   currentpage = newpage;
+   pagarray[currentpage].style.display = "block";
+};
